c6c646b3ccf6884247197b4a2971415c020764ec
Vaibhav-Dodiya <vaibhavdodiya847@gmail.com>
2025-12-03 23:09:16 +0530

    Rebuild frontend with owner dashboard changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1ac33d7d6eb5ef16ee7eb324f73315c0
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "eba4d399c9d8a1506df001f61d69878d",
+"flutter_bootstrap.js": "c6bcfe7b2a580b040f5e131b9b5ee758",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
